b44ec080429f902f4fbd304adb492df14e62ed3d
Azad Furkan ŞAKAR <afsakarr@gmail.com>
2022-05-05 23:27:53 +0300

    Add Tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <welcome-logo />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WelcomeLogo from "@/components/WelcomeLogo.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    WelcomeLogo,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
